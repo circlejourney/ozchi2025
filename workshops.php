4431dfd45084ce6877f4e499a6260218671b22c8
circlejourney <sceptile_rayquaza@hotmail.com>
2025-10-08 11:20:06 +1000

    Improve workshop schedule styling

--- workshops.php
+++ workshops.php
@@ -1,7 +1,7 @@
 <?php
     $title = "Workshop Schedule | OzCHI 2025";
     $description = "Schedules, dates, and bios for the workshops at OzCHI 2025.";
-    $breadcrumbs = ["Program" => null, "Workshops" => "workshops.php"];
+    $breadcrumbs = ["Program" => null, "Workshop Schedule" => "workshops.php"];
     $content = file_get_contents("html/workshop-schedule.html");
     require("template.php");
 ?>
